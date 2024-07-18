cd7fc4f94a0ba30dce6fd99cffc8820d8be289b9
eunji3280 <eunji3280@gmail.com>
2024-07-18 17:11:25 +0900

    오류가있으나 그냥 일단 올려본다

--- src/main/java/com/ejkim/book/Application.java
+++ src/main/java/com/ejkim/book/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         // SpringApplication.run : 내장 WAS 실행
+        //쓸데없는 주석
         SpringApplication.run(Application.class, args);
     }
 }
